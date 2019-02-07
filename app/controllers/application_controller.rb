e04d83921ce304d0f2a6a29c767192e56ef39458
alemat <alematgebru@gmail.com>
2019-02-07 11:19:38 +0300

    user interface modifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   respond_to :html, :js, :json, :pdf
   before_action :set_application_calendar, :set_no_cache
 
