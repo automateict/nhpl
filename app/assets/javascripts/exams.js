d943ab98eaa37840e5d2fd0a59cbfae645adf2c9
G/yohannes Zenebe <gyohannes1@gmail.com>
2019-08-28 09:15:50 +0300

    update

--- app/assets/javascripts/exams.js
+++ app/assets/javascripts/exams.js
@@ -3,10 +3,9 @@ $(function () {
         var id_number = $("#exam_result_id_number").val();
         var first_name = $("#exam_result_first_name").val();
         var father_name = $("#exam_result_father_name").val();
-        var grand_father_name = $("#exam_result_grand_father_name").val();
         $.ajax({
             url: '/applicants/check_exam',
-            data: {id_number: id_number, first_name: first_name, father_name: father_name, grand_father_name: grand_father_name},
+            data: {id_number: id_number, first_name: first_name, father_name: father_name},
             success: function(response){
                 $('#exam_result').html(response)
             }
