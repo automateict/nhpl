7a142fed73d83c1d18c6af3300d5ff2a558f203d
G/yohannes Zenebe <gyohannes1@gmail.com>
2019-09-26 11:24:40 +0300

    update

--- app/models/exam_hub.rb
+++ app/models/exam_hub.rb
@@ -1,9 +1,9 @@
 class ExamHub < ApplicationRecord
-  belongs_to :region
+  belongs_to :region, optional: true
   has_many :applicant_exam_hubs
   has_many :applicants, through: :applicant_exam_hubs
 
-  validates :region_id, :name, :city, :ceiling, presence: true
+  validates :name, presence: true
 
   def total_applicants(academic_year=AcademicYear.current.id)
     applicants.joins(:applicant_exam_hub).where('academic_year_id = ?', academic_year).uniq || []
